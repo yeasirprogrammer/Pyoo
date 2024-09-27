147abfa8c939df46b29eb0ffcc37b497a1239e8c
Md Yeasir Hosen <138030698+mdyeasirhosen@users.noreply.github.com>
2024-09-27 18:30:25 +0600

    added all items final

--- js/home.js
+++ js/home.js
@@ -1,36 +1,49 @@
-// add money to the account
-// S-01. add event Handler
-//      prevent page is reload after submit   '
-// S-02.. get money to be added to the account balance. 
-// step-03.... 
-// Step-01 add event handeler to the add money button inside from
-document.getElementById('btn-add-money')
-.addEventListener('click', function(event){
-    // prevent page is reload after submitget money to be added to the account balance. 
-         event.preventDefault() 
-    //  Step-02  get money to be added to the account balance. 
-    const addMoneyInput = document.getElementById('input-add-money').value 
-    console.log(addMoneyInput);
-    const pinNumberInput= document.getElementById('input-pin-number').value 
-    console.log(pinNumberInput);
-// Step-03 veryfiy pin number! 
-// Worng way to vaildate Pin Number!
-if(pinNumberInput === '1234'){
-    event.preventDefault() 
-    console.log('addeing money to your account');
-    // step -04 current balancse
-     const balance = document.getElementById('accuont-balance').innerText;
-     console.log(balance);
-    //  step-05: addMoney Input with balance
- const addmoneyNumber = parseFloat(addMoneyInput);
- const balanceNumber = parseFloat(balance);
- const newBalance = addmoneyNumber+ balanceNumber;
- console.log(newBalance);
-//  step-6 
-document.getElementById('accuont-balance').innerText = newBalance;
 
-}
-else{
-   alert('faild to add money!please try aggain');
-}
-});
+// add money to the account
+/*
+S-1: add event handler
+prevent page reload after form submit
+s-2: get money to be added to the account balance
+get the pin number
+
+s-3: verify the pin number
+*/
+
+
+// step-1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money')
+    .addEventListener('click', function(event){
+        // prevent page reload after form submit
+        event.preventDefault();
+
+        // step-2: get money to be added to the account
+        const addMoneyInput = document.getElementById('input-add-money').value;
+        console.log(addMoneyInput);
+
+        // get the pin number provided
+        const pinNumberInput = document.getElementById('input-pin-number').value;
+        console.log(typeof pinNumberInput);
+        
+        // step-3: verify pin number
+        // wrong way to validate pin number
+        if(pinNumberInput === '1234'){
+            console.log('adding money to your account');
+
+            // step-4: get the current balance
+            const balance = document.getElementById('account-balance').innerText;
+            console.log(typeof balance)
+
+            // step-5: add addMoneyInput with balance
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);
+
+            // step-6: update the balance in the UI/DOM
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else{
+            alert('Failed to add money! Please try again.')
+        }
+
+    });
